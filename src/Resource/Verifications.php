64895f7186a90cafb001a90718d793911efcd90b
Eric Zhu <eric@zhu.email>
2018-11-15 10:59:20 +0800

    no message

--- src/Resource/Verifications.php
+++ src/Resource/Verifications.php
@@ -17,15 +17,16 @@ class Verifications extends Resource
      * @param int $countryCode
      * @param string $phoneNumber
      * @param string $scene
+     * @param bool $requireAuthorization
      * @return Models\Verification
      * @throws \Acgn\Center\Exceptions\InvalidParamsResponseException
      * @throws \Acgn\Center\Exceptions\HttpTransferException
      * @throws \Acgn\Center\Exceptions\ParseResponseException
      * @throws \Acgn\Center\Exceptions\ResponseException
      */
-    public function create(string $captchaResponse, int $countryCode, string $phoneNumber, string $scene)
+    public function create(string $captchaResponse, int $countryCode, string $phoneNumber, string $scene, bool $requireAuthorization = false)
     {
-        $request = new Request(Request::POST, $this->getPath(), false);
+        $request = new Request(Request::POST, $this->getPath(), $requireAuthorization);
         $request->setBody([
             'captcha_response' => $captchaResponse,
             'country_code' => $countryCode,
